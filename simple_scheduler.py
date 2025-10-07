000e965f43ad746084ab6461a3359607d80ace6b
blankgus <blankgus@gmail.com>
2025-10-07 11:24:19 -0300

    Add files via upload

--- simple_scheduler.py
+++ simple_scheduler.py
@@ -1,13 +1,73 @@
-# simple_scheduler.py (trecho atualizado)
-def gerar_grade(self):
-    # ...
-    for turma_nome, disc_nome in pendentes:
-        atribuido = False
-        profs_possiveis = [p for p in self.professores.values() if disc_nome in p.disciplinas]
-        random.shuffle(profs_possiveis)
+# simple_scheduler.py
+from models import Aula
+from collections import defaultdict
+import random
+
+class SimpleGradeHoraria:
+    def __init__(self, turmas, professores, disciplinas):
+        self.turmas = turmas
+        self.professores = {p.nome: p for p in professores}
+        self.disciplinas = {d.nome: d for d in disciplinas}
+        self.dias = ["seg", "ter", "qua", "qui", "sex"]
+        # Horários sem o recreio (4)
+        self.horarios = [1, 2, 3, 5, 6, 7]
         
-        for prof in profs_possiveis:
-            # 🔑 PULAR HORÁRIO DO RECREIO
-            combinacoes = [(dia, h) for dia in self.dias for h in self.horarios if h != 4]
-            random.shuffle(combinacoes)
-            # ... resto do código ...
+        self.carga_turma = defaultdict(lambda: defaultdict(int))
+        for turma in turmas:
+            for nome_disc, disc in self.disciplinas.items():
+                if turma.serie in disc.series:
+                    self.carga_turma[turma.nome][nome_disc] = disc.carga_semanal
+
+    def gerar_grade(self):
+        aulas = []
+        prof_aulas = defaultdict(list)
+        turma_aulas = defaultdict(list)
+        
+        pendentes = []
+        for turma_nome in self.carga_turma:
+            for disc, carga in self.carga_turma[turma_nome].items():
+                for _ in range(carga):
+                    pendentes.append((turma_nome, disc))
+        
+        random.shuffle(pendentes)
+        
+        for turma_nome, disc_nome in pendentes:
+            atribuido = False
+            profs_possiveis = [p for p in self.professores.values() if disc_nome in p.disciplinas]
+            random.shuffle(profs_possiveis)
+            
+            for prof in profs_possiveis:
+                # Usar horários sem recreio
+                combinacoes = [(dia, h) for dia in self.dias for h in self.horarios]
+                random.shuffle(combinacoes)
+                
+                for dia, horario in combinacoes:
+                    if dia not in prof.disponibilidade:
+                        continue
+                    conflito = False
+                    for a in prof_aulas[prof.nome]:
+                        if a.dia == dia and a.horario == horario:
+                            conflito = True
+                            break
+                    for a in turma_aulas[turma_nome]:
+                        if a.dia == dia and a.horario == horario:
+                            conflito = True
+                            break
+                    if not conflito:
+                        aula = Aula(turma_nome, disc_nome, prof.nome, dia, horario)
+                        aulas.append(aula)
+                        prof_aulas[prof.nome].append(aula)
+                        turma_aulas[turma_nome].append(aula)
+                        atribuido = True
+                        break
+                if atribuido:
+                    break
+            
+            if not atribuido and profs_possiveis:
+                prof = profs_possiveis[0]
+                dia = list(prof.disponibilidade)[0] if prof.disponibilidade else "seg"
+                horario = self.horarios[0]  # Primeiro horário válido
+                aula = Aula(turma_nome, disc_nome, prof.nome, dia, horario)
+                aulas.append(aula)
+        
+        return aulas
