31e6711b2ebe832afaeff4a3bf72a64ffa111abb
blankgus <blankgus@gmail.com>
2025-10-20 00:07:03 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
-# NO database.py, atualize a função criar_dados_iniciais():
+import json
+import os
+from models import Turma, Professor, Disciplina, Sala, Aula
+
+# Arquivo de database
+DB_FILE = "escola_database.json"
 
 def criar_dados_iniciais():
     """Cria dados iniciais para teste"""
@@ -47,42 +52,42 @@ def criar_dados_iniciais():
     disciplinas = [
         # GRUPO A - TURMAS A
         Disciplina("Português A", 5, "pesada", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
-        Disciplina("Matemática A", 5, "pesada", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
-        Disciplina("História A", 2, "media", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
-        Disciplina("Geografia A", 2, "media", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
-        Disciplina("Ciências A", 2, "media", ["6anoA", "7anoA", "8anoA", "9anoA"], "A"),
-        Disciplina("Biologia A", 2, "media", ["1emA", "2emA", "3emA"], "A"),
-        Disciplina("Física A", 2, "pesada", ["1emA", "2emA", "3emA"], "A"),
-        Disciplina("Química A", 2, "pesada", ["1emA", "2emA", "3emA"], "A"),
-        Disciplina("Inglês A", 2, "leve", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
+        Disciplina("Matemática A", 6, "pesada", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
+        Disciplina("História A", 3, "media", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
+        Disciplina("Geografia A", 3, "media", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
+        Disciplina("Ciências A", 3, "media", ["6anoA", "7anoA", "8anoA", "9anoA"], "A"),
+        Disciplina("Biologia A", 3, "media", ["1emA", "2emA", "3emA"], "A"),
+        Disciplina("Física A", 3, "pesada", ["1emA", "2emA", "3emA"], "A"),
+        Disciplina("Química A", 3, "pesada", ["1emA", "2emA", "3emA"], "A"),
+        Disciplina("Inglês A", 3, "leve", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
         Disciplina("Arte A", 2, "leve", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
-        Disciplina("Educação Física A", 2, "pratica", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
+        Disciplina("Educação Física A", 3, "pratica", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
         Disciplina("Filosofia A", 2, "media", ["1emA", "2emA", "3emA"], "A"),
         Disciplina("Sociologia A", 2, "media", ["1emA", "2emA", "3emA"], "A"),
-        Disciplina("Informática A", 2, "leve", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
+        Disciplina("Informática A", 3, "leve", ["6anoA", "7anoA", "8anoA", "9anoA", "1emA", "2emA", "3emA"], "A"),
         Disciplina("Dinâmica A", 1, "leve", ["6anoA", "7anoA", "8anoA", "9anoA"], "A"),
         Disciplina("Vida Pratica A", 1, "leve", ["6anoA", "7anoA", "8anoA", "9anoA"], "A"),
         
         # GRUPO B - TURMAS B
         Disciplina("Português B", 5, "pesada", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
-        Disciplina("Matemática B", 5, "pesada", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
-        Disciplina("História B", 2, "media", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
-        Disciplina("Geografia B", 2, "media", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
-        Disciplina("Ciências B", 2, "media", ["6anoB", "7anoB", "8anoB", "9anoB"], "B"),
-        Disciplina("Biologia B", 2, "media", ["1emB", "2emB", "3emB"], "B"),
-        Disciplina("Física B", 2, "pesada", ["1emB", "2emB", "3emB"], "B"),
-        Disciplina("Química B", 2, "pesada", ["1emB", "2emB", "3emB"], "B"),
-        Disciplina("Inglês B", 2, "leve", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
+        Disciplina("Matemática B", 6, "pesada", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
+        Disciplina("História B", 3, "media", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
+        Disciplina("Geografia B", 3, "media", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
+        Disciplina("Ciências B", 3, "media", ["6anoB", "7anoB", "8anoB", "9anoB"], "B"),
+        Disciplina("Biologia B", 3, "media", ["1emB", "2emB", "3emB"], "B"),
+        Disciplina("Física B", 3, "pesada", ["1emB", "2emB", "3emB"], "B"),
+        Disciplina("Química B", 3, "pesada", ["1emB", "2emB", "3emB"], "B"),
+        Disciplina("Inglês B", 3, "leve", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
         Disciplina("Arte B", 2, "leve", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
         Disciplina("Educação Física B", 3, "pratica", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
         Disciplina("Filosofia B", 2, "media", ["1emB", "2emB", "3emB"], "B"),
         Disciplina("Sociologia B", 2, "media", ["1emB", "2emB", "3emB"], "B"),
-        Disciplina("Informática B", 2, "leve", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
+        Disciplina("Informática B", 3, "leve", ["6anoB", "7anoB", "8anoB", "9anoB", "1emB", "2emB", "3emB"], "B"),
         Disciplina("Dinâmica B", 1, "leve", ["6anoB", "7anoB", "8anoB", "9anoB"], "B"),
         Disciplina("Vida Pratica B", 1, "leve", ["6anoB", "7anoB", "8anoB", "9anoB"], "B"),
     ]
     
-        salas = [
+    salas = [
         Sala("Sala 1", 30, "normal"),
         Sala("Sala 2", 30, "normal"),
         Sala("Sala 3", 30, "normal"),
@@ -270,4 +275,3 @@ def resetar_banco():
         os.remove(DB_FILE)
     init_db()
     return True
-
