50da806a482a10e18d9e82096d5aba0373c63f74
blankgus <blankgus@gmail.com>
2025-12-02 12:24:17 -0300

    Update models.py

--- models.py
+++ models.py
@@ -7,20 +7,21 @@ DIAS_SEMANA = ["seg", "ter", "qua", "qui", "sex"]
 
 # Horários disponíveis por segmento
 HORARIOS_EFII = [1, 2, 3, 4, 5, 6]  # 07:50-12:20 (6 períodos)
-HORARIOS_EM = [1, 2, 3, 4, 5, 6, 7]  # 07:00-12:20 ou 13:10 (7 períodos)
+HORARIOS_EM = [1, 2, 3, 4, 5, 6, 7, 8]  # 07:00-12:20 ou 13:10 (7 períodos)
 
 # Mapeamento de horários reais
 HORARIOS_REAIS = {
     # EF II: 07:50-12:20
-    1: "07:50-08:40",
-    2: "08:40-09:30", 
-    3: "09:30-09:50",  # INTERVALO
-    4: "09:50-10:40",
-    5: "10:40-11:30",
-    6: "11:30-12:20",
+    1: "07:00-07:50",
+    2: "07:50-08:40",
+    3: "08:40-09:30", 
+    4: "09:30-09:50",  # INTERVALO
+    5: "09:50-10:40",
+    6: "10:40-11:30",
+    7: "11:30-12:20",
     
     # EM adicional: 12:20-13:10
-    7: "12:20-13:10"
+    8: "12:20-13:10"
 }
 
 @dataclass
